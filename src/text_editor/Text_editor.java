ca347d68509df1be7b17557952168ef472230afd
Marcus Mobark <marcus.mobark@gmail.com>
2020-11-04 16:32:41 +0100

    Initial Commit: Project + .gitignore

---
+++ src/text_editor/Text_editor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package text_editor;
+
+/**
+ *
+ * @author marcu
+ */
+public class Text_editor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
