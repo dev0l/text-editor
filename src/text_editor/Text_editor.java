7c664621b6fb22cc16a8c6c5951fa19e812f6178
Marcus Mobark <marcus.mobark@gmail.com>
2020-11-24 14:10:36 +0100

    Simple Text Editor Complete

--- src/text_editor/Text_editor.java
+++ src/text_editor/Text_editor.java
@@ -15,7 +15,8 @@ public class Text_editor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
     }
     
 }
